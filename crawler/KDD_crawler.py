41d2bbc00fb901607719e22e6b68be020ac9f722
Sonic <hsnusonic@gmail.com>
2015-06-16 12:09:05 +0800

    fix ascii codec problem

--- crawler/KDD_crawler.py
+++ crawler/KDD_crawler.py
@@ -27,7 +27,7 @@ def str_between(s, first, last):
   return s[start:end]
 
 def find_abstract(s):
-  abstract = str_between(s, "ABSTRACT", "SOURCE MATERIALS")
+  abstract = str_between(s, "ABSTRACT", "AUTHORS")
   abstract = str_between(abstract, "<p>", "</p>")
   return abstract
 
@@ -52,32 +52,51 @@ def get_data(_id):
   abstract_str = find_abstract(res.text)
   refs = find_references(res.text)
   cites = find_citeby(res.text)
-  return _id[0], title, authors, abstract_str, refs, cites
+  return _id, title, authors, abstract_str, refs, cites
 
 def format_paper(paper):
-  inform = []
-  inform.append("<id>{}</id>".format(paper.id))
-  inform.append("<title>{}</title>".format(paper.title))
+  inform = ""
+  inform += "<id>{}</id>".format(paper.id) + '\n'
+  inform += "<title>{}</title>".format(paper.title.encode('utf-8')) + '\n'
   for author in paper.authors:
-    inform.append("<author>{}</author>".format(author))
-  inform.append("<abstract>{}</abstract>".format(paper.abstract))
+    inform += "<author>{}</author>".format(author.encode('utf-8')) + '\n'
+  inform += "<abstract>{}</abstract>".format(paper.abstract) + '\n'
   for ref in paper.refs:
-    inform.append("<ref>{}</ref>".format(ref))
+    inform += "<ref>{}</ref>".format(ref) + '\n'
   for cite in paper.cites:
-    inform.append("<citeby>{}</citeby>".format(cite))
-  return '\n'.join(inform)
+    inform += "<citeby>{}</citeby>".format(cite) + '\n'
+  return inform
 
-def get_ids(url):
+def get_ids(url, filename):
   res = requests.get(url)
   ids_block = str_between(res.text, "Research session 1: location-based services",\
       "Industry &#38; government invited talks")
   pattern = re.compile(ur'citation\.cfm\?id=(\d*?)&')
   ids = re.findall(pattern, ids_block)
+  with open(filename, 'w') as f:
+    f.write('\n'.join(ids))
   return ids
 
 if __name__ == "__main__":
-  url = "http://dl.acm.org/citation.cfm?id=2623628&preflayout=flat"
-  ids = get_ids(url)
+  #url = "http://dl.acm.org/citation.cfm?id=2623628&preflayout=flat"
+  #ids = get_ids(url, "KDD2014_ids")
+  with open("KDD2014_ids", 'r') as f:
+    ids = [_id.strip() for _id in list(f)]
+
+  # data structure to store paper information
   Paper = namedtuple("Paper", ['id', 'title', 'authors', 'abstract', 'refs', 'cites'])
-  paper = Paper._make(get_data(ids[0]))
-  print format_paper(paper)
+
+  with open("fail_list", 'w') as f:
+    papers = []
+    for i in xrange(len(ids)):
+      try:
+        paper = Paper._make(get_data(ids[i]))
+        papers.append(format_paper(paper))
+      except:
+        f.write(ids[i] + '\n')
+        time.sleep(0.5 + random.random())
+
+  with open("KDD2014", 'w') as f:
+    f.write(str(len(papers))+'\n')
+    for paper in papers:
+      f.write(paper)
